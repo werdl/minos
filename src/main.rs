40e606244c73c7282e80af948c5d131699468c1f
werdl <116349156+werdl@users.noreply.github.com>
2024-03-04 14:00:48 +0000

    make it finally bootable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod lib;
+
+use lib::device::io::vga::*;
+
+#[no_mangle]
+pub extern "C" fn _start() {
+    println!("Hello World{}", "!");
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
 }
